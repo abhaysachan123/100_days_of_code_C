c29a5048395fa6fb243841405293feb11fab3b68
abhaysachan123 <abhaysachan1001@gmail.com>
2025-11-12 19:09:38 +0530

    Added day44_q88

---
+++ day44_q88.c
@@ -0,0 +1,25 @@
+// Q88: Replace spaces with hyphens in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+hello world
+Output 1:
+hello-world
+*/
+
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    int i;
+    fgets(s, sizeof(s), stdin);
+
+    for (i = 0; s[i] != '\0'; i++) {
+        if (s[i] == ' ')
+            s[i] = '-';
+    }
+
+    printf("%s", s);
+    return 0;
+}
