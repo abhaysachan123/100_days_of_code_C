923328cbce327e73f36c8c6f0bf6e1873b090a88
abhaysachan123 <abhaysachan1001@gmail.com>
2025-11-17 11:09:17 +0530

    Update day50_qz100.c

--- q91-100/day50_qz100.c
+++ q91-100/day50_qz100.c
@@ -1,10 +1,13 @@
 // Q100: Print all sub-strings of a string.
-//
-// Sample Test Cases:
-// Input 1:
-// abc
-// Output 1:
-// a,ab,abc,b,bc,c
+
+/*
+ Sample Test Cases:
+ Input 1:
+ abc
+ Output 1:
+ a,ab,abc,b,bc,c
+*/
+     
 
 #include <stdio.h>
 #include <string.h>
