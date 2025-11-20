7a9b018e084a39a9cd511bc06ed837486a175935
abhaysachan123 <abhaysachan1001@gmail.com>
2025-11-21 00:43:58 +0530

    Added day61_q111

---
+++ day61_q111.c
@@ -0,0 +1,54 @@
+// Q111: Find the first negative integer in every subarray/window of size k.
+
+/*
+Sample Test Cases:
+Input 1:
+arr[] = [-8, 2, 3, -6, 10], k = 2
+Output 1:
+-8 0 -6 -6
+
+Input 2:
+arr[] = [12, -1, -7, 8, -15, 30, 16, 28], k = 3
+Output 2:
+-1 -1 -7 -15 -15 0
+
+Input 3:
+arr[] = [12, 1, 3, 5], k = 3
+Output 3:
+0 0
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    for (int i = 0; i <= n - k; i++) {
+        int found = 0;
+
+        for (int j = i; j < i + k; j++) {
+            if (arr[j] < 0) {
+                printf("%d ", arr[j]);
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found)
+            printf("0 ");
+    }
+
+    return 0;
+}
